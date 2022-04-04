930606c329ac7e8fda3318141f489deaed4f6793
WuSi <wusixstx@163.com>
2022-04-04 10:12:33 +0800

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,50 @@
+%{
+%   Demonstration of drawing the domain linear inequalities:
+%      Ax <= b, A∈R^{m*2}, b∈R^{m*1}
+%
+%   Written by Wu Si (wusixstx@163.com)
+%}
+clear all; close all; clc;
+
+A = [1 0; 1 1];
+A = A./vecnorm(A')';
+b = [1; 2];
+
+bias        = [0; 0];
+axes_range  = [-5, 5, -5, 5];
+face_color  = 'c';
+legend_name = '';
+
+%% draw static half plane
+figure(1); set(gcf, 'position', [0,0,1200,400]);
+subplot(1, 3, 1); axis equal;
+% draw the domain of the inequalities
+H.f1_domain    = half_plane([], A, b, bias, axes_range, face_color, '');
+
+%% draw the animation of half plane
+% initialize the domain of the inequalities
+subplot(1, 3, 2); axis equal;
+H.f2_domain   = half_plane([], A, b, bias, axes_range, face_color, '');
+
+% initialize the domain of each constraint of the inequalities
+subplot(1, 3, 3); axis equal;
+H.f2_domain_1 = half_plane([], A, b, bias, axes_range, 'r', '');
+H.f2_domain_2 = half_plane([], A, b, bias, axes_range, 'b', '');
+
+% animiation
+for i = 1:100
+    
+    % time-varying inequalities
+    A = [sin(i/100*2*pi), -cos(i/100*2*pi); 
+         cos(i/100*2*pi),  sin(i/100*2*pi)];
+    b = [1; 1];
+    bias = [0; 1];
+
+    % update the graphics
+    H.f2_domain    = half_plane(H.f2_domain, A, b, bias);
+    H.f2_domain_1  = half_plane(H.f2_domain_1, A(1, :), b(1, :), bias);
+    H.f2_domain_2  = half_plane(H.f2_domain_2, A(2, :), b(2, :), bias);
+
+    % drawnow;
+    pause(0.1);
+end
---
+++ half_plane.m
@@ -0,0 +1,106 @@
+%{
+%   Drawing the domain linear inequalities:
+%      Ax <= b, A∈R^{m*2}, b∈R^{m*1}
+%
+%   Written by Wu Si (wusixstx@163.com)
+%}
+function H = half_plane(H, A, b, bias, varargin)
+if(isempty(H))
+    H.p_limit   = varargin{1};      % The boundness of graphic
+    H.P         = [H.p_limit(2) H.p_limit(1) H.p_limit(1) H.p_limit(2); H.p_limit(4) H.p_limit(4) H.p_limit(3) H.p_limit(3)];
+    H.clr       = varargin{2};      % The colors
+    H.name      = varargin{3};      %
+    H.bias      = bias;
+
+    if(~isequal(H.name, ''))
+        H.name_visibility = 'on';
+    else
+        H.name_visibility = 'off';
+    end
+
+    [x, ~] = calcu_polyhedron(A, b, H.P);
+    if(isequal(size(H.clr), [1, 3]))
+        hold on;
+        H.handle_space = patch(x(1, :)+H.bias(1), x(2, :)+H.bias(2), H.clr(:), 'facecolor', H.clr(:), 'facealpha', 0.3, 'displayname', H.name, 'HandleVisibility', H.name_visibility);
+    else
+        hold on;
+        H.handle_space = patch(x(1, :)+H.bias(1), x(2, :)+H.bias(2), H.clr(1), 'facecolor', H.clr(1), 'facealpha', 0.3, 'displayname', H.name, 'HandleVisibility', H.name_visibility);
+    end
+
+    axis(H.p_limit);
+else
+    % Update
+    H.bias      = H.bias;      %
+    [x, ~] = calcu_polyhedron(A, b, H.P);
+    H.handle_space.XData = x(1,:)+H.bias(1);
+    H.handle_space.YData = x(2,:)+H.bias(2);
+end
+end
+
+
+%% The feasible set
+% Ax \le b, A\in R(m,2), b\in R(m,1), P\in R(2, 4)
+function [x, flag] = calcu_polyhedron(A, b, P)
+flag = true;
+m = size(A, 1);
+x = [];
+
+% Coorditnate axis
+for k = 1:m
+    [~, mall] = calcu_halfspace(A(k, :), b(k, :), P);
+    x = [x, mall];
+end
+% Boundary
+for k = 1:m
+    for j = (k+1):m
+        if(abs(det([A(k, :); A(j, :)])) > 1e-6)
+            x = [x, [A(k, :); A(j, :)]\[b(k);b(j)] ];
+        end
+    end
+end
+
+% The valid boundary
+x_right  = x(:, all(A*x-b <= 1e-6, 1));
+x_right  = unique(x_right', 'rows')';
+
+% Obtain the convex hull
+try
+    k = convhull(x_right(1, :),x_right(2, :));
+    x = x_right(:, k);
+catch
+    x    = zeros(2, 1);
+    flag = false;
+    fprintf('[half_space_2D] empty polyhedron \n');
+end
+end
+
+% Ax \le b, A\in R(1,2), b\in R(1,1), P\in R(2, 4)
+function [x, xall] = calcu_halfspace(A, b, P)
+b  = b/norm(A); 
+A  = A/norm(A);
+
+nP = size(P, 2);
+A_P = [ones(nP, 1), zeros(nP, 1); zeros(nP, 1), ones(nP, 1)];
+b_P = [P(1, :), P(2, :)]';
+
+% Boundary points
+xall = P;
+for k = 1:size(A_P, 1)
+    if(abs(det([A_P(k, :); A])) > 1e-6)
+        xall = [xall, [A_P(k, :); A]\[b_P(k);b]];
+    end
+end
+
+% 获取有效的边界
+xall  = xall(:, A*xall-b <= 1e-6);
+% xall  = unique(xall', 'rows')';
+
+try
+    k = convhull(xall(1, :),xall(2, :));
+    x = xall(:, k);
+catch
+    x    = zeros(2, 1);
+    fprintf('[half_plane] empty\n');
+end
+
+end
